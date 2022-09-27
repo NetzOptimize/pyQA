e0d3fa57d1ac6ba8ca9a777f0ed5ff5611d8b788
Sachin4619 <113972430+SachinSoni4619@users.noreply.github.com>
2022-09-27 14:54:18 +0530

    🚀 Added explicit -> element_located_selection_state_to_be
    for CSS
    
    Co-Authored-By: Montek <87750128+Montekkundan@users.noreply.github.com>

--- FUNCTIONS/helper.py
+++ FUNCTIONS/helper.py
@@ -2549,6 +2549,15 @@ class Checker:
                         f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
                     print(error.as_string())
                     assert False, f"{error.as_string()}"
+            elif self.ac == E_ELEMENT_LOCATED_SELECTION_STATE_TO_BE:
+                try:
+                    WebDriverWait(self.driver, self.time).until(
+                        ec.element_located_selection_state_to_be((By.CSS_SELECTOR, f"{self.lv}")))
+                except TimeoutException:
+                    error = NoSuchElementPresent(
+                        f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
+                    print(error.as_string())
+                    assert False, f"{error.as_string()}"
             else:
                 error = IllegalCharError(f"{self.ac}")
                 print(error.as_string())
