788bf5ee775bb18e2ef96f57f58aa48a76886181
Sachin4619 <113972430+SachinSoni4619@users.noreply.github.com>
2022-09-27 13:34:12 +0530

    🚀 Added explicit -> title_is
    For CSS
    
    Co-Authored-By: Montek <87750128+Montekkundan@users.noreply.github.com>

--- FUNCTIONS/helper.py
+++ FUNCTIONS/helper.py
@@ -2423,6 +2423,15 @@ class Checker:
                         f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
                     print(error.as_string())
                     assert False, f"{error.as_string()}"
+            elif self.ac == E_TITTLE_IS:
+                try:
+                    WebDriverWait(self.driver, self.time).until(
+                        ec.title_is((By.CSS_SELECTOR, f"{self.lv}")))
+                except TimeoutException:
+                    error = NoSuchElementPresent(
+                        f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
+                    print(error.as_string())
+                    assert False, f"{error.as_string()}"
             else:
                 error = IllegalCharError(f"{self.ac}")
                 print(error.as_string())
