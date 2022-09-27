4ef5310e9dcd8a81745431b582f7d31cca0660a1
Sachin4619 <113972430+SachinSoni4619@users.noreply.github.com>
2022-09-27 12:35:33 +0530

    🚀 Added explicit -> presence_of_element_located
    Co-Authored-By: Montek <87750128+Montekkundan@users.noreply.github.com>

--- FUNCTIONS/helper.py
+++ FUNCTIONS/helper.py
@@ -2452,6 +2452,14 @@ class Checker:
                         f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
                     print(error.as_string())
                     assert False, f"{error.as_string()}"
+            elif self.ac == E_PRESENCE_OF_ELEMENT_LOCATED:
+                try:
+                    WebDriverWait(self.driver, self.time).until(ec.presence_of_element_located((By.XPATH, f"{self.lv}")))
+                except TimeoutException:
+                    error = NoSuchElementPresent(
+                        f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
+                    print(error.as_string())
+                    assert False, f"{error.as_string()}"
             else:
                 error = IllegalCharError(f"{self.ac}")
                 print(error.as_string())
