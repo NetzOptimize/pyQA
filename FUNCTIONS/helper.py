7f0968162997a25a1b9889dac9a30f1080752e4f
Sachin4619 <113972430+SachinSoni4619@users.noreply.github.com>
2022-09-27 14:49:19 +0530

    🚀 Added explicit ->element_located_to_be_selected
    For CSS
    
    Co-Authored-By: Montek <87750128+Montekkundan@users.noreply.github.com>

--- FUNCTIONS/helper.py
+++ FUNCTIONS/helper.py
@@ -2531,6 +2531,15 @@ class Checker:
                         f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
                     print(error.as_string())
                     assert False, f"{error.as_string()}"
+            elif self.ac == E_ELEMENT_LOCATED_TO_BE_SELECTED:
+                try:
+                    WebDriverWait(self.driver, self.time).until(
+                        ec.element_located_to_be_selected((By.CSS_SELECTOR, f"{self.lv}")))
+                except TimeoutException:
+                    error = NoSuchElementPresent(
+                        f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
+                    print(error.as_string())
+                    assert False, f"{error.as_string()}"
             else:
                 error = IllegalCharError(f"{self.ac}")
                 print(error.as_string())
