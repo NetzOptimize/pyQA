a42b9c6657b04968473fd9adf985f81a112a564f
Sachin4619 <113972430+SachinSoni4619@users.noreply.github.com>
2022-09-27 13:06:23 +0530

    🚀 Added explicit -> frame_to_be_available_and_switch_to_it
    Co-Authored-By: Montek <87750128+Montekkundan@users.noreply.github.com>

--- FUNCTIONS/helper.py
+++ FUNCTIONS/helper.py
@@ -2500,6 +2500,14 @@ class Checker:
                         f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
                     print(error.as_string())
                     assert False, f"{error.as_string()}"
+            elif self.ac == E_FRAME_TO_BE_AVAILABLE_AND_SWITCH_TO_IT:
+                try:
+                    WebDriverWait(self.driver, self.time).until(ec.frame_to_be_available_and_switch_to_it((By.XPATH, f"{self.lv}")))
+                except TimeoutException:
+                    error = NoSuchElementPresent(
+                        f"{self.time} -> {self.ac} -> {self.locator} -> {self.lv}")
+                    print(error.as_string())
+                    assert False, f"{error.as_string()}"
             else:
                 error = IllegalCharError(f"{self.ac}")
                 print(error.as_string())
